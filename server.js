9a5abbb3d27fe965b075b6abe1cd3aa22426e00c
Kevin Sooley <sooleyka@gmail.com>
2018-01-18 11:24:05 -0500

    first pass at serverside fix to get it to connect

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const server = require('socket.io')();
+const http = require('http');
+const io = require('socket.io');
 const firstTodos = require('./data');
 const Todo = require('./todo');
+const port = 3003;
 
-server.on('connection', (client) => {
+var server = http.createServer(function(req, res){
+    res.writeHead(200,{ 'Content-Type': 'text/html'});
+    res.end('<h1>Oh, you found me. Cool.</h1>');
+});
+
+server.listen(port);
+
+var socket = io.listen(server);
+
+socket.on('connection', (client) => {
     // This is going to be our fake 'database' for this application
     // Parse all default Todo's from db
 
     // FIXME: DB is reloading on client refresh. It should be persistent on new client
     // connections from the last time the server was run...
+console.log("this worked");
     const DB = firstTodos.map((t) => {
         // Form new Todo objects
         return new Todo(title=t.title);
@@ -36,4 +48,3 @@ server.on('connection', (client) => {
 });
 
 console.log('Waiting for clients to connect');
-server.listen(3003);
